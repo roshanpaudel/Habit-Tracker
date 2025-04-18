1b27339b079f3ca2d2eb4e1f9f29987863a64f92
roshanpaudel <paudelroshan93@gmail.com>
2025-04-18 22:06:49 +1000

    input displayed in table

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+let taskList = [];
+
 const handleOnSubmit = (e) => {
   const newForm = new FormData(e);
   taskInput = newForm.get("task");
   hoursInput = newForm.get("hours");
-  console.log(taskInput, hoursInput);
+  const obj = {
+    taskInput,
+    hoursInput,
+  };
+  taskList.push(obj);
+  enterList();
+};
+
+const enterList = () => {
+  const listItems = document.getElementById("entryList");
+  let str = "";
+  taskList.map((item, i) => {
+    str += ` <tr><td scope="row">${i + 1}</td>
+                  <td>${item.taskInput}</td>
+                  <td>${item.hoursInput}</td>
+                  <td class="text-end">
+                    <button class="btn btn-danger">
+                      <i class="fa-solid fa-trash"></i>
+                    </button>
+                    <button class="btn btn-success">
+                      <i class="fa-solid fa-arrow-right"></i>
+                    </button> </td></tr>`;
+  });
+  listItems.innerHTML = str;
 };
